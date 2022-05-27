8ba2295e5f0b9e27475501c1806012f6cddeb521
moniad <moni.dziedzic@gmail.com>
2022-05-28 00:00:02 +0200

    Add sentiment analysis of tweets. Save updated tweets to the same file

--- SentimentAnalyser.py
+++ SentimentAnalyser.py
@@ -1,18 +1,35 @@
 import json
 
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-def read_file(filename="bitcointweet.json"):
-    with open(filename, 'r') as f:
+nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+
+
+def update_tweets_with_sentiment(filename="bitcointweet.json"):
+    tweets_with_sentiment = []
+    with open(filename, 'r+') as f:
         tweets = json.load(f)
         for tweet in tweets:
-            print(tweet['text'])
-            analyse_sentiment(tweet['text'])
+            sentiment = analyse_sentiment(tweet['text'])
+            tweet.update({"sentiment": sentiment})
+            tweets_with_sentiment.append(tweet)
+
+            print(tweet)
             print("------------------------------------")
+
+    save_tweets_to_file(filename, tweets_with_sentiment)
+
+
+def save_tweets_to_file(filename, tweets_with_sentiment):
+    with open(filename, 'w') as file:
+        json.dump(tweets_with_sentiment, file)
 
 
 def analyse_sentiment(text):
-    pass
+    return sia.polarity_scores(text)["compound"]
 
 
 if __name__ == "__main__":
-    read_file()
+    update_tweets_with_sentiment()
